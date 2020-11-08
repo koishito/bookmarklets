a9224025805bc342cf47b9f046a21a3e577816db
koishito <64292509+koishito@users.noreply.github.com>
2020-11-08 18:46:03 +0900

    Update FloatMSG.js

--- FloatMSG.js
+++ FloatMSG.js
@@ -1,13 +1,14 @@
 javascript:(function(){
 var e=1.0;
+floatBOX(e);
 function floatBOX(e){
   var a=document.createElement("div");
-  a.style.cssText="position:fixed;top:50%;left:50%;transform: translate(-50%, -50%);padding:50px;background:silver;border:1px solid #aaa;text-align:center;";
+	a.style.cssText="position:fixed;top:150px;left:0;width:100%25;padding:10px;background:silver;border:1px solid #aaa;text-align:center;z-index:19999;";
+  //a.style.cssText="position:fixed;top:50%;left:50%;transform: translate(-50%, -50%);padding:50px;background:silver;border:1px solid #aaa;text-align:center;";
   a.innerHTML=e.toFixed(2)+' 倍速';
   a.style.fontSize = '40px';
   document.body.appendChild(a);
   setTimeout(closenode, 3000);
   function closenode(){a.parentNode.removeChild(a);}
-  };
-floatBOX(e);
+};
 })();
