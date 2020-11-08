49cc074f065ec6eb3047e05b954d63445a151eec
koishito <64292509+koishito@users.noreply.github.com>
2020-11-08 18:46:35 +0900

    Update FloatMSG.js

--- FloatMSG.js
+++ FloatMSG.js
@@ -3,7 +3,7 @@ var e=1.0;
 floatBOX(e);
 function floatBOX(e){
   var a=document.createElement("div");
-	a.style.cssText="position:fixed;top:150px;left:0;width:100%25;padding:10px;background:silver;border:1px solid #aaa;text-align:center;z-index:19999;";
+  a.style.cssText="position:fixed;top:150px;left:0;width:100%25;padding:10px;background:silver;border:1px solid #aaa;text-align:center;z-index:19999;";
   //a.style.cssText="position:fixed;top:50%;left:50%;transform: translate(-50%, -50%);padding:50px;background:silver;border:1px solid #aaa;text-align:center;";
   a.innerHTML=e.toFixed(2)+' 倍速';
   a.style.fontSize = '40px';
