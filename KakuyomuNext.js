b3c781a17c9afb45d6138dab0659051598f587bd
koishito <64292509+koishito@users.noreply.github.com>
2020-11-13 23:05:52 +0900

    Update KakuyomuNext.js

--- KakuyomuNext.js
+++ KakuyomuNext.js
@@ -1,1 +1,17 @@
-
+javascript:(function(){
+  if (!(/^https:\/\/kakuyomu.jp\/works\/\d+\/episodes\/\d+$/.test(location.href))){
+    alert('カクヨム作品ページではありません。　or　次のエピソードはありません。');
+  }else{
+    var lp = location.href.match(/^https:\/\/kakuyomu.jp\/works\/\d+\/episodes\//)[0];
+    var dlinks = document.links;
+    for (var i = dlinks.length-1; i >= 0; i--){
+      var lpn = dlinks[i].href.match(/^https:\/\/kakuyomu.jp\/works\/\d+\/episodes\//)[0];
+      //console.log(lpn);
+      if((dlinks[i].id == 'contentMain-readNextEpisode' ) && (lpn == lp)){
+        alert(dlinks[i].href);
+        nextarticle = dlinks[i].href;
+        break;
+      }
+    }
+  }
+})();
