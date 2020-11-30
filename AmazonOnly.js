a7dfec358ff90e738bbab544b17a24b349d3b36b
koishito <64292509+koishito@users.noreply.github.com>
2020-11-30 22:23:16 +0900

    Update AmazonOnly.js

--- AmazonOnly.js
+++ AmazonOnly.js
@@ -1,7 +1,7 @@
 javascript:(function(){
-  alert(!(/^https:\/\/Amazon\..*\/s\?k=.*$/.test(location.href)));
-  if (!(/^https:\/\/Amazon.*\/.*dp\/.*$/.test(location.href))){
+  alert(/^https:\/\/amazon.*$/.test(location.href));
+  /*if (/^https:\/\/amazon\..*\/s\?k=.*$/.test(location.href)){
     //location.href = location.href + '&emi=AN1VRQENFRJN5';
-    location.href = location.href + '&rh=p_6%3AAN1VRQENFRJN5';
-  }
+    location.href = location.href + '&rh=p_6%253AAN1VRQENFRJN5';
+  }*/
 })();
