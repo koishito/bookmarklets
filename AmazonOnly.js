b9c521784b3c62cf1afe38bb71fd2e3fee02920b
koishito <64292509+koishito@users.noreply.github.com>
2020-11-30 22:09:38 +0900

    Update AmazonOnly.js

--- AmazonOnly.js
+++ AmazonOnly.js
@@ -1,5 +1,5 @@
 javascript:(function(){
-  alert(!(/^https:\/\/Amazon.*\/s\?k=.*$/.test(location.href)));
+  alert(!(/^https:\/\/Amazon\..*\/s\?k=.*$/.test(location.href)));
   if (!(/^https:\/\/Amazon.*\/.*dp\/.*$/.test(location.href))){
     //location.href = location.href + '&emi=AN1VRQENFRJN5';
     location.href = location.href + '&rh=p_6%3AAN1VRQENFRJN5';
