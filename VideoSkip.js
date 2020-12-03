336728fb638178d44471022a24c120d651a21cc4
koishito <64292509+koishito@users.noreply.github.com>
2020-12-03 23:11:27 +0900

    Update VideoSkip.js

--- VideoSkip.js
+++ VideoSkip.js
@@ -2,9 +2,9 @@ javascript:(function(){
   var b=document.querySelector("video[src]");
   if(b!=null){
     var a=document.createElement("div"),f,d,e;
-    a.style.cssText="position:fixed;top:150px;left:0;width:100%25;padding:10px;background:silver;border:1px solid #aaa;text-align:center;z-index:19999;";
+    a.style.cssText="position:fixed;top:0px;left:0;width:100%25;padding:10px;background:teal;border:1px solid #aaa;text-align:center;z-index:19999;";
     a.onclick=function(){a.parentNode.removeChild(a);};
-    z=[0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.5,3.0];
+    z=[-60,-30,-10,-5,5,10,30,60];
     for(f=0;f<z.length;f++){
       e=z[f].toFixed(2);
       d=document.createElement("button");
@@ -18,9 +18,13 @@ javascript:(function(){
     document.body.appendChild(a);
     function c(g){
       g.stopPropagation();
-      b.playbackRate=g.target.value;
+      b.pause();
+      var ct=b.currentTime + g.target.value;
+      ct=ct<0?0:ct;
+      ct=b.duration<ct?b.duration:ct;
+      b.currentTime=ct;
+      b.play();
       console.log(g.target.value);
-      a.click();
     }
   }
 })();
