b5d5cba9d7a4c13432674b7bce458120bfa33829
koishito <64292509+koishito@users.noreply.github.com>
2020-12-19 16:16:34 +0900

    create Video operation(AutoDisp).js and VideoSkip(AutoDisp).js

---
+++ Video operation(AutoDisp).js
@@ -0,0 +1,79 @@
+javascript:(function(){
+  var b=document.querySelector("video[src]");
+  if(b!=null){
+    function ElementRegist() {
+      var a=document.createElement("div"),f,d,e;
+      a.style.cssText="visibility:visible;position:fixed;top:0px;left:0px;width:100%;padding:4px;background:teal;border:1px solid #aaa;text-align:center;z-index:19999;";
+      a.style.fontSize = "20px";
+      a.onclick=function(){a.parentNode.removeChild(a);};
+      a.id="mainiam";
+      z=["x0.5","x0.75","x1.0","x1.25","x1.5","x1.75","x2.0",-60,-30,-10,-5,5,10,30,60];
+      for(f=0;f<z.length;f++){
+        e=z[f];
+        d=document.createElement("button");
+        d.type="button";
+        d.innerHTML=e;
+        d.style.cssText="background:silver;padding:4px 10px;margin:8px;";;
+        if (isNaN(e)) {d.style.cssText="margin:8px;background-color: "+ (b.playbackRate == e.slice(1) ? "#99FFCC;" : "gray;");};
+        d.value=e;
+        d.onclick=c;
+        a.appendChild(d);
+        a.appendChild(document.createTextNode("|"))
+      }
+      
+      document.body.appendChild(a);
+
+      function c(g){
+        e = g.target.value;
+        g.stopPropagation();
+        if (isNaN(e)) {
+          e=e.slice(1);
+          b.playbackRate = e;
+          console.log(e,a,g);
+          sto = setTimeout(closenode, 1000);
+          function closenode(){a.click()};
+          ElementRegist();
+        } else {
+          var ps=!b.paused;
+          if (ps) {b.pause();}
+          console.log(b.currentTime,parseInt(e));
+          var ct=b.currentTime + parseInt(e);
+          ct = (ct < 0) ? 0 : ct;
+          ct = (b.duration) < ct ? b.duration : ct;
+          b.currentTime = ct;
+          if (ps) {b.play();}
+          console.log(e,ct);
+        }
+      }
+
+      window.addEventListener("mousemove", event => {
+        // console.log("screen:" + event.screenY);
+        // console.log("client:" + event.clientY);
+        // console.log("page  :" + event.pageY);
+        // console.log("offset:" + event.offsetY);
+        document.getElementById("mainiam").style.visibility = (event.clientY < 50) ? "visible" : "hidden";
+      });
+
+      document.addEventListener("fullscreenchange", ()=>{
+        if( window.document.fullscreenElement ){
+          console.log("フルスクリーン開始");
+          sto = setTimeout(closenode, 1000);
+          function closenode(){a.click()};
+          ElementRegist();
+        }
+        else{
+          console.log("フルスクリーン終了");
+          sto = setTimeout(closenode, 1000);
+          function closenode(){a.click()};
+          ElementRegist();
+        }
+      });
+
+    }
+    ElementRegist();
+
+
+
+  }
+
+})();
---
+++ VideoSkip(AutoDisp).js
@@ -0,0 +1,39 @@
+javascript:(function(){
+  var b=document.querySelector("video[src]");
+  if(b!=null){
+    var a=document.createElement("div"),f,d,e;
+    a.style.cssText="visibility:visible;position:fixed;top:0px;left:0px;width:100%;padding:4px;background:teal;border:1px solid #aaa;text-align:center;z-index:19999;";
+    a.onclick=function(){a.parentNode.removeChild(a);};
+    a.id="mainiam";
+    z=[-60,-30,-10,-5,5,10,30,60];
+    for(f=0;f<z.length;f++){
+      e=z[f];
+      d=document.createElement("button");
+      d.type="button";
+      d.innerHTML=e;
+      d.style.cssText="background:silver;padding:4px 10px;margin:4px 20px;";
+      d.value=e;
+      d.onclick=c;
+      a.appendChild(d);
+    }
+    document.body.appendChild(a);
+    function c(g){
+      g.stopPropagation();
+      var ps=!b.paused;
+      if (ps) {b.pause();}
+      console.log(b.currentTime,parseInt(g.target.value));
+      var ct=b.currentTime + parseInt(g.target.value);
+      ct=ct<0?0:ct;
+      ct=b.duration<ct?b.duration:ct;
+      b.currentTime=ct;
+      if (ps) {b.play();}
+      console.log(g.target.value,ct);
+    }
+
+    window.addEventListener("mousemove", event => {
+      /*console.log(event.clientY);*/
+      document.getElementById("mainiam").style.visibility = (event.clientY < 50) ? "visible" : "hidden";
+    });
+  }
+
+})();
