cc3c86136cd7660a222afdab9a2f56a1c293bb63
koishito <64292509+koishito@users.noreply.github.com>
2021-01-05 16:59:18 +0900

    created ClickByPushedKey.js

---
+++ ClickByPushedKey.js
@@ -0,0 +1,66 @@
+javascript:(function(){
+
+  var obj = {setting : true};
+
+  document.body.addEventListener('keydown', event => {
+    console.log(obj.setting);
+    if (obj.setting) {
+      if (event.key == 'Escape') {
+        obj.setting = false;
+        alert(objList());
+      } else {
+        var clientX = parseInt(sessionStorage.getItem('clientX'));
+        var clientY = parseInt(sessionStorage.getItem('clientY'));
+        targetElement = document.elementFromPoint(clientX, clientY);
+        obj[event.key] = targetElement;
+        alert("setting key[  " + event.key + '  ] :\n ' + targetElement);
+      }
+    } else {
+      var targetElement = obj[event.key];
+      if (typeof targetElement != "undefined") {
+        targetElement.click();
+      }
+    }
+  });
+
+  document.body.addEventListener("mousemove", function(e){
+    if (obj.setting) {
+      sessionStorage.setItem('clientX', e.clientX);
+      sessionStorage.setItem('clientY', e.clientY);
+    }
+  });
+
+  function objList() {
+    let ret = "Setting Finished";
+    for(let key in obj){
+      ret += (key == 'setting') ? '' : '\n\n key[  ' + key + '  ] :\n ' + obj[key];
+    }
+    return ret;
+  }
+
+})();
+
+  // sessionStorage.setItem('key', '');
+
+  // document.body.addEventListener('keydown', event => {
+  //   var key = sessionStorage.getItem('key');
+  //   if (!key) {
+  //     sessionStorage.setItem('key', event.key);
+  //     console.log(event.key);
+  //   } else if (event.key == key) {
+  //     var clientX = parseInt(sessionStorage.getItem('clientX'));
+  //     var clientY = parseInt(sessionStorage.getItem('clientY'));
+  //     targetElement = document.elementFromPoint(clientX, clientY);
+  //     console.log(event.key, targetElement);
+  //     targetElement.click();
+  //   }
+
+  // });
+
+  // document.body.addEventListener("mousemove", function(e){
+  //   if (!obj.Escape) {
+  //     sessionStorage.setItem('clientX', e.clientX);
+  //     sessionStorage.setItem('clientY', e.clientY);
+  //   }
+  // });
+
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # bookmarklets
 
-notice:Don't Use //.
+**notice:Don't Use // in code. /\* \*/ used.**
+
+### ClickByPushedKey.js
+Assign mouse clicks to keystrokes.
+
+1. After execution, it will be in registration mode.
+1. If you press the replace key with the mouse cursor placed on the target element, the registration key and the element will be linked.
+1. Press the escape key to exit the registration mode.
+1. When you press the register key, the element is clicked.
+
