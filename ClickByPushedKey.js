4d75a3f1ad4fde9fbf90eb32e2f4913d85baa305
koishito <64292509+koishito@users.noreply.github.com>
2021-01-20 19:53:57 +0900

    Fixeda few bugs.

--- ClickByPushedKey.js
+++ ClickByPushedKey.js
@@ -13,7 +13,7 @@ javascript:(function(){
         var clientY = parseInt(sessionStorage.getItem('clientY'));
         targetElement = document.elementFromPoint(clientX, clientY);
         obj[event.key] = targetElement;
-        alert("setting key[  " + event.key + '  ] :\n ' + targetElement);
+        alert("setting key[  " + event.key + ' , ' + clientX + ' , ' + clientY + ' ] :\n ' + targetElement);
       }
     } else {
       var targetElement = obj[event.key];
