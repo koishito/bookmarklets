afeebc6af1e62e70f561e5c67e61caaa6877e0c5
koishito <64292509+koishito@users.noreply.github.com>
2021-05-24 20:55:23 +0900

    The date and advantage conditions are highlighted by coloring.

--- hellowWork.js
+++ hellowWork.js
@@ -3,6 +3,29 @@ javascript:(function(){
   if (document.title != `ハローワークインターネットサービス - 求人情報検索・一覧`) {return;}
   document.body.style.zoom='95%';
 
+  /*日付を強調*/
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+  let yesterdayDate = 'YYYY年MM月DD日'.replace(/YYYY/, date.getFullYear())
+                                      .replace(/MM/, date.getMonth() + 1)
+                                      .replace(/DD/, date.getDate());
+  var date = new Date();
+  let todayDate = 'YYYY年MM月DD日'.replace(/YYYY/, date.getFullYear())
+                                  .replace(/MM/, date.getMonth() + 1)
+                                  .replace(/DD/, date.getDate());
+  const fs13_ml01 = document.getElementsByClassName(`fs13 ml01`);
+  for (let i = 0; i < fs13_ml01.length; i++) {
+    if (fs13_ml01[i].innerText == yesterdayDate) {fs13_ml01[i].style.cssText += `background-color:green;`;}
+    if (fs13_ml01[i].innerText == todayDate) {fs13_ml01[i].style.cssText += `background-color:aqua;`;}
+  }
+
+  /*利点を強調*/
+  let nes_label = document.getElementsByClassName(`nes_label any fs07 mt04`);
+  for (let i = 0; i < nes_label.length; i++) {
+    nes_label[i].style.cssText += `background-color:#FFCBCB;`;
+  }
+
+
   let item = 0;
   /*let dir = 0;
   let scrollOffset = 5;*/
@@ -18,6 +41,7 @@ javascript:(function(){
       let scrollTop = window.pageYOffset + ClientRectTop - headerHight/* - (dir * scrollOffset)*/;
       console.log(`scrollTop : ${scrollTop}`);
       document.documentElement.scrollTop = scrollTop - 10;
+      element.getElementsByClassName(`fb`)[0].innerText=`(${item + 1}/${kyujin.length}) 職種`;
     /*}, 100);*/
   }
 
