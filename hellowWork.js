1959a19553ca0f80fb0a961c308972ea56d863d7
koishito <64292509+koishito@users.noreply.github.com>
2021-05-24 16:43:23 +0900

    Tentatively completed hellowWork.js

--- hellowWork.js
+++ hellowWork.js
@@ -3,28 +3,38 @@ javascript:(function(){
   if (document.title != `ハローワークインターネットサービス - 求人情報検索・一覧`) {return;}
   document.body.style.zoom='95%';
 
-  let p = 0;
-  let kyujin = document.getElementsByClassName("kyujin_foot");
-  console.log(`kyujinLength : ${kyujin.length}`);
+  let item = 0;
+  /*let dir = 0;
+  let scrollOffset = 5;*/
+  let headerHight = document.getElementById(`top_header`).clientHeight;
+  let kyujin = document.getElementsByClassName(`kyujin mt1 noborder`);
 
-  kyujin[p].scrollIntoView({block: "end"});
+  scrollKyujin(kyujin[item]);
+
+  function scrollKyujin(element) {
+    /*setTimeout(() => {*/
+      element.scrollIntoView({block: `end`});
+      let ClientRectTop = element.getBoundingClientRect().top;
+      let scrollTop = window.pageYOffset + ClientRectTop - headerHight/* - (dir * scrollOffset)*/;
+      console.log(`scrollTop : ${scrollTop}`);
+      document.documentElement.scrollTop = scrollTop - 10;
+    /*}, 100);*/
+  }
 
   document.body.addEventListener('keydown', event => {
-    setTimeout(abc, 5);
+    /*dir = (event.key == 'ArrowDown') - (event.key == 'ArrowUp');*/
+    dir = (event.key == 'ArrowLeft') - (event.key == 'ArrowRight');
+    if (dir == 0) {return;}
 
-    function abc() {
-      let dir = (event.key == 'ArrowDown') - (event.key == 'ArrowUp');
-      if (dir == 0) {return;}
-
-      p += dir;
-      console.log(`page : ${p}`);
-      if (p == -2) {document.getElementsByName("fwListNaviBtnPrev")[0].click();}
-      else if (p == -1) {document.getElementsByClassName("flex page_navi")[0].scrollIntoView({block: "center"});}
-      else if (p == kyujin.length) {document.getElementsByClassName("flex page_navi")[1].scrollIntoView({block: "center"});}
-      else if (p == kyujin.length + 1) {document.getElementsByName("fwListNaviBtnNext")[0].click();}
-      else if (dir == -1 ) {kyujin[p].scrollIntoView({block: "end"});}
-      else {kyujin[p].scrollIntoView({block: "end"});}
-
+    if ((typeof kyujin[item] == `undefined`) && (Math.sign(item) == dir)) {
+      let tagName = (dir == -1) ? `fwListNaviBtnPrev` : `fwListNaviBtnNext`;
+      document.getElementsByName(tagName)[0].click();
+    } else {
+      item += dir;
+      console.log(`item : ${item}`);
+      if (item == -1) {document.getElementsByClassName(`flex page_navi`)[0].scrollIntoView({block: `center`});}
+      else if (item == kyujin.length) {document.getElementsByClassName(`flex page_navi`)[1].scrollIntoView({block: `center`});}
+      else {scrollKyujin(kyujin[item]);}
     }
 
   });
