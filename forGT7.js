265630ab2d98171a70abd2262fa161e8a2a10ce0
z89nmer <z89nmer56@yahoo.co.jp>
2022-03-14 21:42:25 +0900

    ADD for GT7

---
+++ forGT7.js
@@ -0,0 +1,33 @@
+/*document.getElementById('leftMenu').style.width = '300px';
+document.getElementsByClassName('manual_detail')[0].style.width = '650px';
+document.getElementById('content').style.width = '650px';
+
+sele = "#content";
+var i, j, sheets, rules, rule = null;
+sheets = document.styleSheets;
+for(i=0; i<sheets.length; i++){
+    // そのstylesheetが持つCSSルールのリストを取得
+    rules = sheets[i].cssRules;
+    for(j=0; j<rules.length; j++){
+        // セレクタが一致するか調べる
+        if(sele === rules[j].selectorText){
+            rule = rules[j];
+            break;
+        }
+    }
+}
+console.log(rule);
+
+_1lSA1b _3yX2NS
+
+document.styleSheets[0].cssRules[33].style.width = '650px';
+*/
+
+javascript:(function(){
+    if (location.href == `https://www.gran-turismo.com/jp/gt7/apex/`) {
+        document.styleSheets[0].cssRules[33].style.width = '650px';
+    }
+    if (location.href == `https://www.gran-turismo.com/jp/gt7/manual/`) {
+        document.styleSheets[1].cssRules[33].style.width = '650px';
+    }
+})();
