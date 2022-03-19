84d70286a96471705aa4cca5987d0923b71e63d3
z89nmer <z89nmer56@yahoo.co.jp>
2022-03-19 10:23:31 +0900

    YouTube_HiddenItems: Reduced hidden parts

--- YouTube_HiddenItems.js
+++ YouTube_HiddenItems.js
@@ -3,8 +3,7 @@ javascript:(function(){
     document.getElementsByTagName(`ytd-toggle-button-renderer`)[1].style.display = "none";
     document.getElementsByTagName(`ytd-button-renderer`)[0].style.display = "none";
 
-    document.getElementsByClassName(`style-scope ytd-menu-renderer force-icon-button style-default size-default`)[0].style.display = "none";
-    document.getElementsByClassName(`style-scope ytd-menu-renderer force-icon-button style-default size-default`)[1].style.display = "none";
+/*    document.getElementsByClassName(`style-scope ytd-menu-renderer force-icon-button style-default size-default`)[1].style.display = "none";
     document.getElementsByClassName(`style-scope ytd-menu-renderer force-icon-button style-default size-default`)[2].style.display = "none";
-    document.getElementsByClassName(`style-scope ytd-menu-renderer force-icon-button style-default size-default`)[3].style.display = "none";
+    document.getElementsByClassName(`style-scope ytd-menu-renderer force-icon-button style-default size-default`)[3].style.display = "none";*/
 })();
