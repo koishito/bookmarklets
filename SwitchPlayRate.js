a8f8afbb68a09ce530157717ed2bb5a96152cab0
z89nmer <z89nmer56@yahoo.co.jp>
2022-04-06 10:18:48 +0900

    SwitchPlayRate: Changed judgment word 0 -> null

--- SwitchPlayRate.js
+++ SwitchPlayRate.js
@@ -3,16 +3,14 @@ javascript:(function(){
   const b=document.querySelector("video[src]");
   const cr=b.playbackRate;
   if(b!=null){
-    d=a.concat(a[0]).reduce((p,c)=>(p==0)?c:((p==c)?0:p),cr);
+    d=a.concat(a[0]).reduce((p,c)=>(p===null)?c:((p==c)?null:p),cr);
     b.playbackRate=d;
     floatBOX(d.toFixed(2)+' 倍速');
   }
   function floatBOX(e){
     var a=document.createElement("div");
-    a.style.cssText="position:fixed;top:150px;left:0;width:25%25;padding:10px;background:silver;border:1px solid #aaa;text-align:center;z-index:19999;";
-    /*a.style.cssText="position:fixed;top:50%;left:50%;transform: translate(-50%, -50%);padding:50px;background:silver;border:1px solid #aaa;text-align:center;";*/
+    a.style.cssText="font-Size:40px;position:fixed;top:150px;left:0;width:auto;padding:10px;background:silver;border:1px solid #aaa;text-align:center;z-index:19999;";
     a.innerHTML=e;
-    a.style.fontSize = '40px';
     document.body.appendChild(a);
     setTimeout(closenode, 3000);
     function closenode(){a.parentNode.removeChild(a);}
